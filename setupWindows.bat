8f13aa65c7beb5e302df759654694d4771185469
S1mon <jaforsi.kozak@gmail.com>
2024-08-08 19:36:11 +0200

    updated setupWindows.bat to delete git repository

--- setupWindows.bat
+++ setupWindows.bat
@@ -2,7 +2,6 @@
 
 git submodule init
 git submodule update
-git remote remove origin
 
 copy main\Compiler.py compi.py
 copy vm\VM.py vm.py
@@ -10,5 +9,6 @@ copy vm\VM.py vm.py
 rd /s /q main
 rd /s /q vm
 del /s /q .gitmodules
+del /s /q .git/
 
 del /q setupWindows.bat
