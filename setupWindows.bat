b2d720e9ec77acb505ecd428671ed3f8defb857c
S1mon <jaforsi.kozak@gmail.com>
2024-08-18 06:47:45 +0200

    fixed setupWindows.bat

--- setupWindows.bat
+++ setupWindows.bat
@@ -7,7 +7,7 @@ git clone https://github.com/baabnq/libs
 copy compiler\Compiler.py compi.py
 copy vm\VM.py vm.py
 
-rd /s /q main
+rd /s /q compiler
 rd /s /q vm
 del /s /q .git/
 del /s /q README.md
